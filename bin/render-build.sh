de34f77568427c928c6be4a9da2b0c14248314d2
DiegoSilvaR <diegosilvarodriguez@hotmail.com>
2023-07-09 18:25:44 -0400

    12

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,7 +5,5 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-bundle exec rake bundle:install
 bundle exec rake db:migrate
-bundle exec rails db:devise
 
