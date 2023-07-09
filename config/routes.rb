6572a5ccf76b9e5f132ba0cac1911af890f5d156
DiegoSilvaR <diegosilvarodriguez@hotmail.com>
2023-07-09 17:28:12 -0400

    9

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  resources :comments
   resources :posts, except: [:index]
   get "posts",  to: "posts#index", as: "user_root"
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: "users/sessions",
+    registrations: "users/registrations"
+  }
   get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
